f49d3ed100421a7a01141aa0cbf1804bafd07dcd
Ashley <ashley.tjonhing@gmail.com>
2017-09-29 22:49:11 -0400

    cost commit 1

--- AshleyCOMP304Lab2/app/src/main/java/com/example/ashleytjon_hing_comp304lab2/RestaurantActivity.java
+++ AshleyCOMP304Lab2/app/src/main/java/com/example/ashleytjon_hing_comp304lab2/RestaurantActivity.java
@@ -29,7 +29,16 @@ public class RestaurantActivity extends AppCompatActivity {
         menu1.setOnClickListener(new View.OnClickListener()  {
 
             public void onClick (View v) {
-                menu1.isChecked()
+                menu1.isChecked() ? (cost = cost + Double.parseDouble(menu1.getText().toString())): cost = cost;
+            }
+        });
+
+        check_button.setOnClickListener(new View.OnClickListener()  {
+
+            public void onClick (View v) {
+                tv.setText(check_button.isChecked() ?
+                        "This option is checked" :
+                        "This option is not checked");
             }
         });
 
@@ -77,52 +86,71 @@ public class RestaurantActivity extends AppCompatActivity {
                 title.setText(R.string.chiR1);
                 menu1.setText(R.string.chiF1);
                 menu2.setText(R.string.chiF2);
-                cost1.setText((double)R.string.chiC1);
+                cost1.setText(R.string.chiC1);
+                cost2.setText(R.string.chiC2);
                 break;
             case "Grand Lake":
                 title.setText(R.string.chiR2);
                 menu1.setText(R.string.chiF3);
                 menu2.setText(R.string.chiF4);
+                cost1.setText(R.string.chiC3);
+                cost2.setText(R.string.chiC4);
                 break;
             case "Bombay Buffet":
                 title.setText(R.string.indR1);
                 menu1.setText(R.string.indF1);
                 menu2.setText(R.string.indF2);
+                cost1.setText(R.string.indC1);
+                cost2.setText(R.string.indC2);
                 break;
             case "Golden Spoon":
                 title.setText(R.string.indR2);
                 menu1.setText(R.string.indF3);
                 menu2.setText(R.string.indF4);
+                cost1.setText(R.string.indC3);
+                cost2.setText(R.string.indC4);
                 break;
             case "Irma Buffet":
                 title.setText(R.string.worldR1);
                 menu1.setText(R.string.worldF1);
                 menu2.setText(R.string.worldF2);
+                cost1.setText(R.string.worldC1);
+                cost2.setText(R.string.worldC2);
                 break;
             case "Circle of Food":
                 title.setText(R.string.worldR2);
                 menu1.setText(R.string.worldF3);
                 menu2.setText(R.string.worldF4);
+                cost1.setText(R.string.worldC3);
+                cost2.setText(R.string.worldC4);
                 break;
             case "Spring Sushi":
                 title.setText(R.string.jpnR1);
                 menu1.setText(R.string.jpnF1);
                 menu2.setText(R.string.jpnF2);
+                cost1.setText(R.string.jpnC1);
+                cost2.setText(R.string.jpnC2);
                 break;
             case "Santouka Ramen":
                 title.setText(R.string.jpnR2);
                 menu1.setText(R.string.jpnF3);
                 menu2.setText(R.string.jpnF4);
+                cost1.setText(R.string.jpnC3);
+                cost2.setText(R.string.jpnC4);
                 break;
             case "McDonalds":
                 title.setText(R.string.usaR1);
                 menu1.setText(R.string.usaF1);
                 menu2.setText(R.string.usaF2);
+                cost1.setText(R.string.usaC1);
+                cost2.setText(R.string.usaC2);
                 break;
             case "Cheesecake Factory":
                 title.setText(R.string.usaR2);
                 menu1.setText(R.string.usaF3);
                 menu2.setText(R.string.usaF4);
+                cost1.setText(R.string.usaC3);
+                cost2.setText(R.string.usaC4);
                 break;
         }
         menu1.setVisibility(View.VISIBLE);
